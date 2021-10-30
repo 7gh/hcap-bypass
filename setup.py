9e30dcb1aca87698dc3ad5ed21e09dbfd3c3c6a5
7gh <88673481+7gh@users.noreply.github.com>
2021-10-30 14:35:29 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from distutils.core import setup
 setup(
-  name = 'hcap-bypass',         # How you named your package folder (MyLib)
-  packages = ['hcap-bypass'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'A module that solves hcaptcha challenges!',   # Give a short description about your library
-  author = 'Luke Cool',                   # Type in your name
-  author_email = 'droidslying@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['hcaptcha', 'bypass', 'hcap', 'hcap bypass', 'bypass hcaptcha'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name = 'hcap-bypass',
+  packages = ['hcap-bypass'],
+  version = '0.1',
+  license='MIT',        
+  description = 'A module that solves hcaptcha challenges!',
+  author = '7gh',  
+  author_email = 'droidslying@gmail.com',
+  url = 'https://github.com/7gh/hcap-bypass',
+  keywords = ['hcaptcha', 'bypass', 'hcap', 'hcap bypass', 'bypass hcaptcha'],
+  install_requires=[        
           'math',
           'base64',
           'httpx',
@@ -19,8 +18,8 @@ setup(
           'json'
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers', 
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.6'
